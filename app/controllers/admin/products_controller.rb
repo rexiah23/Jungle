4b2920fc2b2f1a7cb2344fa25535824ea1d07e06
Lewis Lee <lewisforbusiness@gmail.com>
2021-10-15 11:51:41 -0400

    Finish HTTP basic auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
-
+  http_basic_authenticate_with name: "Jungle", password: "book"
+  before_action :set_post, only: [:destroy]
+  
   def index
     @products = Product.order(id: :desc).all
   end
