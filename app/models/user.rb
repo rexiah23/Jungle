a412adfdabad8bf8834f6cf71dbd5bc5e50625a9
Lewis Lee <lewisforbusiness@gmail.com>
2021-10-19 13:30:53 -0400

    Finish rspec for password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :password, length: { minimum: 5 }
+  validates :password_confirmation, length: { minimum: 5 }
 
   has_secure_password
 
